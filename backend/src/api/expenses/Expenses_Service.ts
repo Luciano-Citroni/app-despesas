a72d5d637237870829ba33acc284790f9fe75c6a
Luciano Citroni Lopes <luciano.citroni227@gmail.com>
2023-12-20 17:09:26 -0300

    implementação dos serviços

---
+++ backend/src/api/expenses/Expenses_Service.ts
@@ -0,0 +1,101 @@
+import { AppDataSource } from '../../utils/data_source';
+import { Expenses } from './Expenses_Model';
+
+type ExpensesRequest = {
+    id?: string;
+    name?: string;
+    description?: string;
+    price?: number;
+    fkUser?: string;
+    payday?: Date;
+    statusPayment: boolean;
+    dataCreated?: Date;
+};
+
+export class ExpensesService {
+    private repository = AppDataSource.getRepository(Expenses);
+
+    async create({ name, description, price, fkUser, payday }: ExpensesRequest): Promise<String | Error> {
+        try {
+            const expense = this.repository.create({
+                name,
+                description,
+                price,
+                fkUser,
+                payday,
+            });
+
+            await this.repository.save(expense);
+
+            return expense.id;
+        } catch (err) {
+            return new Error(err);
+        }
+    }
+
+    async getByID({ id }: ExpensesRequest): Promise<Expenses | Error> {
+        try {
+            const expense = await this.repository.findOneBy({ id: id });
+
+            if (!expense) {
+                return new Error('Expense does not exist');
+            }
+
+            return expense;
+        } catch (err) {
+            return new Error(err);
+        }
+    }
+
+    async getAll(): Promise<Expenses[] | Error> {
+        try {
+            const expense = await this.repository.find();
+
+            if (!expense) {
+                return new Error('There are no registered expenses');
+            }
+
+            return expense;
+        } catch (err) {
+            return new Error(err);
+        }
+    }
+
+    async update({ id, statusPayment, description, name, payday, price }: ExpensesRequest): Promise<Expenses | Error> {
+        try {
+            const expense = await this.repository.findOneBy({ id: id });
+
+            if (!expense) {
+                return new Error('Expense does not exist');
+            }
+
+            expense.statusPayment = statusPayment ? statusPayment : expense.statusPayment;
+            expense.description = description ? description : expense.description;
+            expense.name = name ? name : expense.name;
+            expense.payday = payday ? payday : expense.payday;
+            expense.price = price ? price : expense.price;
+
+            await this.repository.save(expense);
+
+            return expense;
+        } catch (err) {
+            return new Error(err);
+        }
+    }
+
+    async delete({ id }: ExpensesRequest): Promise<Boolean | Error> {
+        try {
+            const expense = await this.repository.findOneBy({ id: id });
+
+            if (!expense) {
+                return new Error('Expense does not exist');
+            }
+
+            await this.repository.remove(expense);
+
+            return true;
+        } catch (err) {
+            return new Error(err);
+        }
+    }
+}
---
+++ backend/src/api/users/Users_Service.ts
@@ -0,0 +1,96 @@
+import { AppDataSource } from '../../utils/data_source';
+import { Users } from './Users_Model';
+
+type UsersRequest = {
+    id?: string;
+    name?: string;
+    email?: string;
+    password?: string;
+};
+
+export class UserService {
+    private repository = AppDataSource.getRepository(Users);
+
+    async create({ name, email, password }: UsersRequest): Promise<String | Error> {
+        try {
+            if (await this.repository.findOneBy({ email: email })) {
+                return new Error('Email already registered');
+            }
+
+            const user = this.repository.create({
+                name: name,
+                email: email,
+                password: password,
+            });
+
+            await this.repository.save(user);
+
+            return user.id;
+        } catch (err) {
+            return new Error(err);
+        }
+    }
+
+    async getByID({ id }: UsersRequest): Promise<Users | Error> {
+        try {
+            const user = await this.repository.findOneBy({ id: id });
+
+            if (!user) {
+                return new Error('User does not exist');
+            }
+
+            return user;
+        } catch (err) {
+            return new Error(err);
+        }
+    }
+
+    async getByEmail({ email }: UsersRequest): Promise<Users | Error> {
+        try {
+            const user = await this.repository.findOneBy({ email: email });
+
+            if (!user) {
+                return new Error('User does not exist');
+            }
+
+            return user;
+        } catch (err) {
+            return new Error(err);
+        }
+    }
+
+    async update({ id, email, password }: UsersRequest): Promise<Users | Error> {
+        try {
+            const user = await this.repository.findOneBy({ id: id });
+
+            if (!user) {
+                return new Error('User does not exist');
+            }
+
+            user.email = email ? email : user.email;
+            user.password = password ? password : user.password;
+
+            await this.repository.save(user);
+
+            return user;
+        } catch (err) {
+            return new Error(err);
+        }
+    }
+
+    async delete({ id }: UsersRequest): Promise<Boolean | Error> {
+        try {
+            const user = await this.repository.findOneBy({ id: id });
+
+            if (!user) {
+                return new Error('User does not exist');
+            }
+
+            await this.repository.remove(user);
+
+            return true;
+        } catch (err) {
+            return new Error(err);
+        }
+    }
+}
